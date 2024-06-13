bdb6c06fcf35a6c77ec83157f5dc1a4308882b16
bottles10 <henryaovare@gmail.com>
2024-06-13 04:57:15 +0000

    Add user habits relations and add username to users table

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,2 +1,15 @@
 class Habit < ApplicationRecord
+    belongs_to :user, counter_cache: :habits_count, touch: true
+
+    
+    validates :name, presence: true
+    validates :count, presence: true
+
+    scope :ordered, -> { order(updated_at: :desc) }
+
+     before_update :check_count
+
+    def check_count
+        self.count = 0 if count < 0
+    end
 end
