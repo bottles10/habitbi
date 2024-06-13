463f30efdaf728e0b050f2cd2ac4bafb3b47c7c9
bottles10 <henryaovare@gmail.com>
2024-06-13 05:13:07 +0000

    Add all old views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
         date.strftime("%v %H:%M %p")
     end
 
-    # def render_turbo_stream_flash_messages
-    #     turbo_stream.prepend 'flash_message', partial: 'layouts/flashes' 
-    # end
+    def render_turbo_stream_flash_messages
+        turbo_stream.prepend 'flash_message', partial: 'layouts/flashes' 
+    end
 end
