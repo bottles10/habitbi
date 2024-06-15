23067002ead1f9c4efb2e1f25f1ca126c2e8e746
bottles10 <henryaovare@gmail.com>
2024-06-15 02:30:44 +0000

    Add google fonts and extend tailwindcss font family

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  # TODO: Fix flash notification timeOut for turbo stream
+  # COMPLETED: Fix flash notification timeOut for turbo stream
   # TODO: Implement awards for habits
-  # TODO: Implement user cannot plus more than date last count and current date
-  # TODO: Add no habits banner if no habits presents
+  # COMPLETED: Implement user cannot plus more than date last count and current date
+  # COMPLETED Add no habits banner if no habits presents
   # TODO: Style session pages
   # TODO: show habits count
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        nosifer: ["Nosifer", "sans-serif"],
+        megrim: ["Megrim", "sans-serif"],
+        rubik: ["Rubik Puddles", "sans-serif"],
       },
     },
   },
