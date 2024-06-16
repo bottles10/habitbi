a16db4886231a13757be428b816d36603b629b07
bottles10 <henryaovare@gmail.com>
2024-06-16 18:48:37 +0000

    Update logo title to root path link

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  # FIXME: Fix header, navigation and notifiction position
+  # COMPLETED: Fix header, navigation and notifiction position
   # COMPLETED: Implement awards for habits
-  # TODO: Minus count removes award if less than required count to have award
   # COMPLETED: Implement user cannot plus more than date last count and current date
   # COMPLETED Add no habits banner if no habits presents
-  # TODO: Style session pages
+  # COMPLETED: Style session pages
+  # TODO: Minus count removes award if less than required count to have award
   # TODO: show habits count
+  # TODO: Add pagination for habits{ limit to 5 }
 end
