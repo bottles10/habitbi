4934cb327b3b9f0c65d7d90592ca9c860df5d6d9
bottles10 <henryaovare@gmail.com>
2024-06-16 00:40:06 +0000

    Style devise pages

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   # FIXME: Fix header, navigation and notifiction position
-  # TODO: Implement awards for habits
+  # COMPLETED: Implement awards for habits
+  # TODO: Minus count removes award if less than required count to have award
   # COMPLETED: Implement user cannot plus more than date last count and current date
   # COMPLETED Add no habits banner if no habits presents
   # TODO: Style session pages
